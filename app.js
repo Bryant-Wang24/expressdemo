27f2d9f4db0eec6b268c8fe8ec1bc75323db1c97
Bryant-Wang24 <485969746@qq.com>
2021-09-29 22:47:55 +0800

    中间件接受表单post数据

--- app.js
+++ app.js
@@ -1,68 +1,80 @@
 const express = require('express')
 const app = express()
+const ejs = require('ejs')
 
 
-// app.set("view engine", "ejs")
 
-// // 配置静态web目录
-// app.use(express.static("static"))
+app.set("view engine", "ejs")
+
+// 配置静态web目录
+app.use(express.static("static"))
+
+// 直接使用express代替 bodyParser中间件 进行调用
+app.use(express.urlencoded({ extended: false }))// 表单请求
+app.use(express.json())// json请求
 
 
-// app.get('/', function (req, res) {
-//     let title = "你好ejs"
-//     res.render("index", {
-//         title: title
-//     })
-//     // res.send('你好，express')
+app.get('/', function (req, res) {
+    // let title = "你好ejs"
+    // res.render("index", {
+    //     title: title
+    // })
+    res.send('首页')
+})
+app.get('/login', function (req, res) {
+    res.render("login", {})
+    // let title = {
+    //     username: '王秋爽',
+    //     age: 23
+    // }
+    // let h = "<h3>我是h3标题</h3>"
+    // let list = ['3333333', '44444444', '555555']
+    // res.render("news", {
+    //     title: title,
+    //     h: h,
+    //     tags: true,
+    //     score: 60,
+    //     list: list
+    // })
+    // res.send('你好，express')
+})
+app.post("/doLogin", (req, res) => {
+    let body = req.body
+    console.log(body);
+    res.send("执行提交")
+})
+
+// // 4.内置中间件
+// app.use(express.static("static"))//可以匹配到base.css
+// // 4.内置中间件
+
+
+// // 1.应用级中间件（主要用在权限判断）
+// app.use((req, res, next) => {//所有的路由匹配之间都会先走到这一步
+//     console.log(new Date());
+//     next()
 // })
-// app.get('/news', function (req, res) {
-//     let title = {
-//         username: '王秋爽',
-//         age: 23
-//     }
-//     let h = "<h3>我是h3标题</h3>"
-//     let list = ['3333333', '44444444', '555555']
-//     res.render("news", {
-//         title: title,
-//         h: h,
-//         tags: true,
-//         score: 60,
-//         list: list
-//     })
-//     // res.send('你好，express')
+// // 1.应用级中间件（主要用在权限判断）
+
+
+
+// // 2.路由级中间件
+// app.get('/edit/add', function (req, res, next) {
+//     console.log('执行增加新闻');
+//     next()//添加next之后他就可以继续向下匹配
 // })
-
-// 4.内置中间件
-app.use(express.static("static"))//可以匹配到base.css
-// 4.内置中间件
-
-
-// 1.应用级中间件（主要用在权限判断）
-app.use((req, res, next) => {//所有的路由匹配之间都会先走到这一步
-    console.log(new Date());
-    next()
-})
-// 1.应用级中间件（主要用在权限判断）
+// app.get('/edit/:id', function (req, res) {
+//     const id = req.params['id'];
+//     res.send('动态路由' + id)
+// })
+// // 2.路由级中间件
 
 
-
-// 2.路由级中间件
-app.get('/edit/add', function (req, res, next) {
-    console.log('执行增加新闻');
-    next()//添加next之后他就可以继续向下匹配
-})
-app.get('/edit/:id', function (req, res) {
-    const id = req.params['id'];
-    res.send('动态路由' + id)
-})
-// 2.路由级中间件
-
-
-// 3.错误处理中间件
-app.use((req, res, next) => {
-    res.status(404).send("404")
-})
-// 3.错误处理中间件
+// // 3.错误处理中间件
+// app.use((req, res, next) => {
+//     res.status(404).send("404")
+// })
+// // 3.错误处理中间件
 
 
 
