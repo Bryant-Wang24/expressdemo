eece8b0a1d2ec77256842b99d02a25fa9f8d1be1
Bryant-Wang24 <485969746@qq.com>
2021-10-07 14:09:45 +0800

    按照日期生成上传文件目录

--- model/tools.js
+++ model/tools.js
@@ -1,14 +1,21 @@
 
 const multer = require('multer')
 const path = require('path')
+var sd = require('silly-datetime');
+const mkdirp = require('mkdirp')
 // const upload = multer({ dest: 'static/uploads' })
 
 let tools = {
     multer() {
         const storage = multer.diskStorage({
             // 配置上传的目录
-            destination: function (req, file, cb) {
-                cb(null, '\static/uploads')//上传之前目录必须存在
+            destination: async function (req, file, cb) {
+                // 1、获取当前日期
+                let day = sd.format(new Date(), 'YYYYMMDD')
+                let dir = path.join('\static/uploads', day)
+                // 按照日期生成图片存储目录
+                await mkdirp(dir)
+                cb(null, dir)//上传之前目录必须存在
             },
             // 修改上传后的文件名
             filename: function (req, file, cb) {
