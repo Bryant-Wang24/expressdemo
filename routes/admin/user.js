c2d9910da44b42ad1cbc4c47c29c1568affa17f5
Bryant-Wang24 <485969746@qq.com>
2021-10-07 14:27:21 +0800

    使用multer多文件上传

--- routes/admin/user.js
+++ routes/admin/user.js
@@ -1,17 +1,25 @@
 const express = require("express")
+const tools = require("../../model/tools")
 const router = express.Router()
 
 router.get("/", (req, res) => {
     res.send("用户列表")
 })
 router.get("/add", (req, res) => {
-    res.send("增加用户")
+    res.render("admin/user/add")
 })
+
+
 router.get("/edit", (req, res) => {
     res.send("修改用户")
 })
-router.post("/doadd", (req, res) => {
-    res.send("执行增加")
+
+const cpUpload = tools.multer().fields([{ name: 'uploadedfile1', maxCount: 1 }, { name: 'uploadedfile2', maxCount: 8 }])
+router.post("/doadd", cpUpload, (req, res) => {
+    res.send({
+        body: req.body,
+        files: req.files
+    })
 })
 router.post("/doedit", (req, res) => {
     res.send("执行修改")
